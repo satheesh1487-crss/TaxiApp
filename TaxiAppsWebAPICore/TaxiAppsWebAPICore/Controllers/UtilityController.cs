7f04a4b2c4ce77e50e5ec24981dc7e79acd62dee
satheesh1487-crss <67497437+satheesh1487-crss@users.noreply.github.com>
2020-07-18 17:47:53 +0530

    Create UtilityController.cs

---
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Controllers/UtilityController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaxiAppsWebAPICore.Models;
+using TaxiAppsWebAPICore.TaxiModels;
+
+namespace TaxiAppsWebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilityController : ControllerBase
+    {
+        private readonly TaxiAppzDBContext _context;
+        public UtilityController(TaxiAppzDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("getCountry")]
+        [Authorize]
+        public IActionResult GetCountry()
+        {
+            List<CountryList> countryList = new List<CountryList>();
+            var countryData = _context.TabCountry.ToList();
+            foreach (var country in countryData)
+            {
+                countryList.Add(new CountryList()
+                {
+                    CountryId = country.CountryId,
+                    CountryCode = country.Code,
+                    CountryName= country.Name,
+                    MobileCode = country.DCode,
+                });
+            }
+            return this.OK<List<CountryList>>(countryList);
+        }
+
+
+        //TODO:: Please add table name 
+        [HttpGet]
+        [Route("getCurrency")]
+        [Authorize]
+        public IActionResult GetCurrency()
+        {
+            List<CurrencyList> curList = new List<CurrencyList>();
+            var countryData = _context.Tab.ToList();
+            foreach (var currency in curList)
+            {
+                curList.Add(new CurrencyList()
+                {
+                    CurrencyId = currency.CurrencyId,
+                    CurrencyName = currency.CurrencyName
+                    
+                });
+            }
+            return this.OK<List<CurrencyList>>(curList);
+        }
+
+    }
+}
