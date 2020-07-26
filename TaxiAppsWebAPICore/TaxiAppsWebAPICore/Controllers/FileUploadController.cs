9f0d8274f6f24114faadf777deafea04ea5b334b
Sundar <sundargsoft@gmail.com>
2020-07-26 10:08:22 +0530

    test

---
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Controllers/FileUploadController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaxiAppsWebAPICore.TaxiModels;
+
+namespace TaxiAppsWebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly TaxiAppzDBContext _context;
+        public FileUploadController(TaxiAppzDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
