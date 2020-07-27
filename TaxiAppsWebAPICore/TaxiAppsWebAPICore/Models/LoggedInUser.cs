bd914f8b121d724eb45c64201b1ff3bbe6162f68
satheesh1487-crss <67497437+satheesh1487-crss@users.noreply.github.com>
2020-07-27 14:28:11 +0530

    Create LoggedInUser.cs

---
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Models/LoggedInUser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxiAppsWebAPICore.Models
+{
+    public class LoggedInUser
+    {
+        public string UserName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string RoleName { get; set; }
+ 
+    }
+}
