f4cbd869648269ce1089c2948435e78fa110a17f
Sundar <sundargsoft@gmail.com>
2020-07-28 10:12:45 +0530

    test

--- TaxiAppsWebAPICore/TaxiAppsWebAPICore/Models/SetPrivilege.cs
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Models/SetPrivilege.cs
@@ -18,29 +18,29 @@ namespace TaxiAppsWebAPICore
     }
     public class GrandParentMenuhierarchy
     {
-       [JsonProperty("grandParentMenuname")]
+       [JsonProperty("name")]
         public string GrandParentMenuname { get; set; }
         [JsonProperty("menuid")]
         public long? Menuid { get; set; }
         [JsonProperty("viewstate")]
         public bool? ViewState { get; set; }
-        [JsonProperty("parentMenuhierarchy")]
+        [JsonProperty("children")]
         public List<ParentMenuhierarchy> parentMenuhierarchies { get; set; }
     }
     public class ParentMenuhierarchy
     {
-        [JsonProperty("parentMenuname")]
+        [JsonProperty("name")]
         public string ParentMenuname { get; set; }
         [JsonProperty("menuid")]
         public long? Menuid { get; set; }
         [JsonProperty("viewstate")]
         public bool? ViewState { get; set; }
-        [JsonProperty("childMenuhierarchy")]
+        [JsonProperty("children")]
         public List<ChildMenuhierarchy> childMenuhierarchies { get; set; }
     }
     public class ChildMenuhierarchy
     {
-        [JsonProperty("childMenuname")]
+        [JsonProperty("name")]
         public string ChildMenuname { get; set; }
         [JsonProperty("menuid")]
         public long? Menuid { get; set; }
