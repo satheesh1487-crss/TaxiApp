81af4e859c6c0e8ac4dd36653e6ef375e113764d
satheesh <satheesh1487@outlook.com>
2020-08-11 22:54:52 +0530

    FAQ FILES ADDED

---
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Controllers/FAQController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaxiAppsWebAPICore.DataAccessLayer;
+using TaxiAppsWebAPICore.Models;
+using TaxiAppsWebAPICore.TaxiModels;
+
+namespace TaxiAppsWebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FAQController : ControllerBase
+    {
+        //jhfuyfryufhgyhtguy
+        private readonly TaxiAppzDBContext _context;
+        public FAQController(TaxiAppzDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("listFAQ")]
+        [Authorize]
+        public IActionResult ListFAQ()
+        {
+            DAFAQ dAFAQ = new DAFAQ();
+            return this.OK<List<ManageFAQList>>(dAFAQ.ListFAQ(_context));
+        }
+
+        //TODO:: Duplicate record check
+        [HttpPut]
+        [Route("editFAQ")]
+        [Authorize]
+        public IActionResult EditFAQ([FromBody] ManageFAQList manageFAQList)
+        {
+            DAFAQ dAFAQ = new DAFAQ();
+            return this.OKResponse(dAFAQ.EditFAQ(_context, manageFAQList, User.ToAppUser()) ? "Updated Successfully" : "Updation Failed");
+        }
+
+        [HttpGet]
+        [Route("getbyFAQId")]
+        [Authorize]
+        public IActionResult GetbyFAQId(long id)
+        {
+            DAFAQ dAFAQ = new DAFAQ();
+            return this.OK<ManageFAQList>(dAFAQ.GetbyFAQId(_context, id));
+        }
+
+        [HttpPut]
+        [Route("statusFAQ")]
+        [Authorize]
+        public IActionResult StatusFAQ(long id, bool isStatus)
+        {
+            DAFAQ dAFAQ = new DAFAQ();
+            return this.OKResponse(dAFAQ.StatusFAQ(_context, id, isStatus, User.ToAppUser()) ? "Active Successfully" : "InActive Successfully");
+        }
+
+        //TODO:: check parent record is deleted
+        [HttpDelete]
+        [Route("deleteFAQ")]
+        [Authorize]
+        public IActionResult DeleteFAQ(long id)
+        {
+            DAFAQ dAFAQ = new DAFAQ();
+            return this.OKResponse(dAFAQ.DeleteFAQ(_context, id, User.ToAppUser()) ? "Deleted Successfully" : "Deletion Failed");
+        }
+    }
+}
---
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Models/ManageFAQList.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxiAppsWebAPICore.Models
+{
+    public class ManageFAQList
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("servicelocid")]
+        public long ?Servicelocid { get; set; }
+
+        [JsonProperty("FAQ_Question")]
+        public string FAQ_Question { get; set; }
+
+        [JsonProperty("FAQ_Answer")]
+        public string FAQ_Answer { get; set; }
+
+        [JsonProperty("complaint_Type")]
+        public string Complaint_Type { get; set; }
+
+        [JsonProperty("isActive")]
+        public bool ?IsActive { get; set; }
+    }
+}
