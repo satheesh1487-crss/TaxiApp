5973558adb02cf556f1def15c2b7a84aee20d3c1
Sundar <sundargsoft@gmail.com>
2020-08-12 01:42:33 +0530

    test

--- TaxiAppsWebAPICore/TaxiAppsWebAPICore/Controllers/LoginController.cs
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Controllers/LoginController.cs
@@ -20,53 +20,27 @@ namespace TaxiAppsWebAPICore.Controllers
         {
          // _context = context;
             _token = token;
-        }      
-
+        }
+        /// <summary>
+        /// Use to Generate Fresh JWT Token
+        /// </summary>
+        /// <returns></returns>
         [HttpPost("LoginUser"),AllowAnonymous]
 
         public IActionResult Login([FromBody] LoginRequest admin)
         {
             return this.OK<UserInfo>(_token.GenerateJWTTokenDtls(admin));
-            //UserInfo userInfo = new UserInfo();
-            //DARoles dARoles = new DARoles();
-            //List<TabAdmin> adminlist = _context.TabAdmin.ToList();
-            //var user = new UserInfo();
-            //var IQAdmin = _context.TabAdmin.Include(a => a.RoleNavigation).Where(a => a.Email.ToLower().Contains(admin.Email.ToLower()) && a.Password == admin.Password).FirstOrDefault();
-            //if (IQAdmin != null)
-            //{
-            //    var tokenString = Extention.GenerateJWTToken(IQAdmin, _context);
-            //    user = new UserInfo() { Email = admin.Email, RememberToken = tokenString,
-            //        Role = IQAdmin.RoleNavigation.RoleName,
-            //        Menukey = dARoles.GetMenukey(IQAdmin.RoleNavigation.RoleName,_context),
-            //        ExpireDate = TimeZoneInfo.ConvertTimeToUtc(DateTime.Now.AddMinutes(300)),InsertedDate = IQAdmin.CreatedAt };
-            //    bool updatetoken = Extention.UpdateToken(IQAdmin.Id,user, _context);
-
-            //    return this.OK<UserInfo>(user);
-            //}
-            //return this.OKFailed("Invalid Credentials");
+         
         }
-        //    return response;
+        /// <summary>
+        /// Use to Re-Generate JWT Token once expried current expired time is 5 mins
+        /// </summary>
+        /// <returns></returns>
         [HttpPost("RegenerateToken"), AllowAnonymous]
         public IActionResult RegenerateToken(string refreshtoken,string emailid)
         {
             return this.OK<UserInfo>(_token.ReGenerateJWTTokenDtls(refreshtoken,emailid));
-            //UserInfo userInfo = new UserInfo();
-            //DARoles dARoles = new DARoles();
-            //List<TabAdmin> adminlist = _context.TabAdmin.ToList();
-            //var user = new UserInfo();
-            //var IQAdmin = _context.TabAdmin.Include(a => a.RoleNavigation).Where(a => a.Email.ToLower().Contains(admin.Email.ToLower()) && a.Password == admin.Password).FirstOrDefault();
-            //if (IQAdmin != null)
-            //{
-            //    var tokenString = Extention.GenerateJWTToken(IQAdmin, _context);
-            //    user = new UserInfo() { Email = admin.Email, RememberToken = tokenString,
-            //        Role = IQAdmin.RoleNavigation.RoleName,
-            //        Menukey = dARoles.GetMenukey(IQAdmin.RoleNavigation.RoleName,_context),
-            //        ExpireDate = TimeZoneInfo.ConvertTimeToUtc(DateTime.Now.AddMinutes(300)),InsertedDate = IQAdmin.CreatedAt };
-            //    bool updatetoken = Extention.UpdateToken(IQAdmin.Id,user, _context);
-
-            //    return this.OK<UserInfo>(user);
-            //}
-            //return this.OKFailed("Invalid Credentials");
+         
         }
     }
 }
