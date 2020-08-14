6bdbec99f9c489f9d10ba2041d81f6c7fd23b6fc
Sundar <sundargsoft@gmail.com>
2020-08-15 00:50:32 +0530

    test

---
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Models/SurgePrice.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaziappzMobileWebAPI.Models
+{
+    public class SurgePrice
+    {
+        [JsonProperty("surgepriceid")]
+        public long? Surgepriceid { get; set; }
+        [JsonProperty("zoneid")]
+        public long? Zoneid { get; set; }
+        [JsonProperty("surgepricetype")]
+        public string Surgepricetype { get; set; }
+        [JsonProperty("starttime")]
+        public string Starttime { get; set; }
+        [JsonProperty("endtime")]
+        public string Endtime { get; set; }
+        [JsonProperty("peaktype")]
+        public string Peaktype { get; set; }
+        [JsonProperty("surgepricevalue")]
+        public string Surgepricevalue { get; set; }
+    }
+}
