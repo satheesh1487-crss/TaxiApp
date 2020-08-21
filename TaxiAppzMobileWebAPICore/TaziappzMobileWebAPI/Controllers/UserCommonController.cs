e91ba8681a9962fabb273c49685baffac8a2e40a
satheesh <satheesh1487@outlook.com>
2020-08-21 22:06:46 +0530

    user common sos and faq added

--- TaxiAppzMobileWebAPICore/TaziappzMobileWebAPI/Controllers/UserCommonController.cs
+++ TaxiAppzMobileWebAPICore/TaziappzMobileWebAPI/Controllers/UserCommonController.cs
@@ -38,11 +38,7 @@ namespace TaziappzMobileWebAPI.Controllers
         #endregion
 
 
-        #region Common-profile
-        /// <summary>
-        /// Hard Coded Data
-        /// </summary>
-        /// <returns></returns>
+        #region Common-profile        
         [HttpPost]
         [Authorize]
         [Route("profile")]
@@ -51,45 +47,31 @@ namespace TaziappzMobileWebAPI.Controllers
             DAUserCommon dAUserCommon = new DAUserCommon();
             List<UserProfileModel> userProfiles = new List<UserProfileModel>();
             userProfiles = dAUserCommon.GetProfile(_context, id, User.ToAppUser());
-            return this.OK<UserProfileModel>(userProfiles, userProfiles.Count == 0 ? "pro det not found" : "data found", userProfiles.Count == 0 ? 0 : 1);          
+            return this.OK<UserProfileModel>(userProfiles, userProfiles.Count == 0 ? "Profile data not found" : "Profie Data Found", userProfiles.Count == 0 ? 0 : 1);          
         }
         #endregion
 
-        #region Common-zonesos
-        /// <summary>
-        /// Hard Coded Data
-        /// </summary>
-        /// <returns></returns>
+        #region Common-zonesos       
         [HttpPost]
         [Route("zonesos")]
-        public IActionResult ZoneSos(GeneralModel generalModel)
+        public IActionResult ZoneSos(long id, decimal lang, decimal lat)
         {
-            List<UserSosModel> userSosModels = new List<UserSosModel>();
-            UserSosModel userSosModel = new UserSosModel();
-            userSosModel.sos = new List<SosUser>();
-            userSosModel.User_Sos = new List<User_Sos>();
-            List<User_Sos> usersos = new List<User_Sos>();
-            List<SosUser> userSos = new List<SosUser>();
-            userSosModel.sos.Add(new SosUser() { Id = 1, name = "CEO", number = "9632596589" });
-            return this.OKRESPONSE<UserSosModel>(userSosModel, userSosModel == null ? "User_Sos_Not_Found" : "User_Sos_found");
+            DAUserCommon dAUserCommon = new DAUserCommon();            
+            List<UserSosModel> userSosModels = new List<UserSosModel>();            
+            userSosModels = dAUserCommon.List(_context,id, lang, lat);
+            return this.OK<UserSosModel>(userSosModels, userSosModels.Count == 0 ? "User SOS Details Not Found" : "User SOS Details Found", userSosModels.Count == 0 ? 0 : 1);            
         }
         #endregion
 
-        #region Common-faq_list
-        /// <summary>
-        /// Hard Coded Data
-        /// </summary>
-        /// <returns></returns>
+        #region Common-faq_list        
         [HttpPost]
         [Route("faq_list")]
-        public IActionResult Faq_List(GeneralModel generalModel)
+        public IActionResult Faq_List(long id, decimal lang, decimal lat, string type)
         {
-            List<UserFaqListModel> userFaqListModels = new List<UserFaqListModel>();
-            UserFaqListModel userFaqListModel = new UserFaqListModel();
-            userFaqListModel.Faq_List = new List<User_Faq_List>();
-            List<User_Faq_List> userfaq = new List<User_Faq_List>();
-            userFaqListModel.Faq_List.Add(new User_Faq_List() { Id = 1, Question = "DOB?", Answer = "257994" });
-            return this.OKRESPONSE<UserFaqListModel>(userFaqListModel, userFaqListModel == null ? "FAQ_List_Not_Found" : "FAQ_List_found");
+            DAUserCommon dAUserCommon = new DAUserCommon();
+            List<UserFaqListModel> userFaqLists = new List<UserFaqListModel>();
+            userFaqLists = dAUserCommon.FaqList(_context, id, lang, lat, type);
+            return this.OK<UserFaqListModel>(userFaqLists, userFaqLists.Count == 0 ? "FAQ_List_Not_Found" : "FAQ_List_found", userFaqLists.Count == 0 ? 0 : 1);
         }
         #endregion
     }
--- TaxiAppzMobileWebAPICore/TaziappzMobileWebAPI/DALayer/DAUserCommon.cs
+++ TaxiAppzMobileWebAPICore/TaziappzMobileWebAPI/DALayer/DAUserCommon.cs
@@ -34,7 +34,7 @@ namespace TaziappzMobileWebAPI.DALayer
                 userProfileModel.Add(new UserProfileModel()
                 {
                     User = userProfile
-                });                
+                });
                 return userProfileModel;
             }
             catch (Exception ex)
@@ -42,6 +42,68 @@ namespace TaziappzMobileWebAPI.DALayer
                 Extention.insertlog(ex.Message, "Admin", "GetProfile", context);
                 return null;
             }
+        }
+
+        public List<UserSosModel> List(TaxiAppzDBContext context, long id, decimal lang, decimal lat)
+        {
+            try
+            {
+                List<UserSosModel> userSosModels = new List<UserSosModel>();
+                List<SosUser> sosUsers = new List<SosUser>();
+                var listZone = context.TabSos.Where(t => t.IsDeleted == 0 && t.IsActive == 1).ToList().OrderByDescending(t => t.UpdatedAt);
+                foreach (var zone in listZone)
+                {
+                    sosUsers.Add(new SosUser()
+                    {
+                        Id = zone.Sosid,
+                        name = zone.Sosname,
+                        number = zone.ContactNumber,
+                    });
+                }
+                userSosModels.Add(new UserSosModel()
+                {
+                    sos = sosUsers
+                });
+                return userSosModels;
+
+            }
+            catch (Exception ex)
+            {
+                Extention.insertlog(ex.Message, "Admin", "List", context);
+                return null;
+            }
+
+        }
+
+        public List<UserFaqListModel> FaqList(TaxiAppzDBContext context, long id, decimal lang, decimal lat, string type)
+        {
+            try
+            {
+                List<UserFaqListModel> userFaqLists = new List<UserFaqListModel>();
+                List<User_Faq_List> user_Faqs = new List<User_Faq_List>();
+                var listFaq = context.TabFaq.Where(t => t.IsDelete == false && t.IsActive == true).ToList().OrderByDescending(t => t.UpdatedAt);
+                foreach (var faq in listFaq)
+                {
+                    user_Faqs.Add(new User_Faq_List()
+                    {
+                        Id=faq.Faqid,
+                        Question=faq.FaqQuestion,
+                        Answer=faq.FaqAnswer                        
+                    });
+                }
+                userFaqLists.Add(new UserFaqListModel()
+                {
+                    Faq_List = user_Faqs
+                });
+                return userFaqLists;
+
+            }
+            catch (Exception ex)
+            {
+                Extention.insertlog(ex.Message, "Admin", "FaqList", context);
+                return null;
+            }
+
         }
     }
 }
