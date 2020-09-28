647f686bec71c282fe334c92245e4720aefc0def
satheesh <satheesh1487@outlook.com>
2020-09-28 10:26:47 +0530

    settings added

--- TaxiAppsWebAPICore/TaxiAppsWebAPICore/Controllers/SettingsController.cs
+++ TaxiAppsWebAPICore/TaxiAppsWebAPICore/Controllers/SettingsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TaxiAppsWebAPICore.DataAccessLayer;
+using TaxiAppsWebAPICore.Helper;
 using TaxiAppsWebAPICore.Models;
 using TaxiAppsWebAPICore.TaxiModels;
 
@@ -44,6 +45,33 @@ namespace TaxiAppsWebAPICore.Controllers
             {
                 return this.KnowOperationError(ex.Message);
             }
+        }
+
+        //TODO:: Duplicate record check
+        [HttpPost]
+        [Route("save")]
+        [Authorize]
+        public IActionResult Save([FromBody] SettingsModel settingsModel)
+        {
+            try
+            {
+                //Validator.validateAddService(serviceInfo);
+                DASettings dASettings = new DASettings();
+                return this.OKResponse(dASettings.SaveSettings(_context, settingsModel, User.ToAppUser()) ? "Inserted Successfully" : "Insertion Failed");
+            }
+            catch (DataValidationException ex)
+            {
+                return this.KnowOperationError(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("getbySettingsId")]
+        [Authorize]
+        public IActionResult GetbySettingsId()
+        {
+            DASettings dASettings = new DASettings();
+            return this.OK<SettingsModel>(dASettings.GetbySettingsId(_context));
         }
     }
 }
